916e1c942927222ed6af4aed622c320b8412faf9
CaptainHook <moradisarah2001@gmail.com>
2023-04-01 01:49:13 +0330

    initilize first config

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className='main-container'>
+        <div className='menubar-container'>Menu Content</div>
+        <div className='content-container'>
+          <div className='header-container'>Header Content</div>
+          Movie Content
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
